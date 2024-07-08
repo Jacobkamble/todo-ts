150fef3dbb2433985c2a8387e57dd2dc57a315e3
Jacob Kamble <jacobkamble@gmail.com>
2024-07-08 22:10:00 +0530

    first commit

---
+++ src/router.tsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter, RouteObject } from "react-router-dom";
+import Applayout from "./layout/applayout";
+import Home from "./pages/home";
+
+const routes: RouteObject[] = [
+  {
+    element: <Applayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+];
+
+const router=createBrowserRouter(routes)
+
+export default router
